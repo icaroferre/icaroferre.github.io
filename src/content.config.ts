a0ce864d0f18bb49032dcc66b2f07b1ca3f7d949
Icaro Ferre <icaroferre@gmail.com>
2026-07-21 13:01:42 -0300

    Added basic blog support

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,18 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const projects = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    project_url: z.string().url().optional(),
+    front_image: z.string(),
+    year: z.union([z.string(), z.number()]).optional(),
+    images: z.array(z.string()).optional(),
+    youtube: z.array(z.string()).optional(),
+    canonical: z.string().optional(),
+  }),
+});
 
 // Define a "blog" collection with schema validation
 
@@ -16,4 +30,19 @@ const music = defineCollection({
     tracklist: z.array(z.string()).optional(),
   }),
 });
-export const collections = { music };
+const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      image: image().optional(),
+    }),
+});
+
+export const collections = { projects, music, blog };
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,9 @@
+---
+title: 'First post'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: '2022-07-08'
+image: './images/FractionsFragments.jpg'
+---
+
+
+Test
